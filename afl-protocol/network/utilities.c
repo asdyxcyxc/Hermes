d1c3accc4635df530452d253c8d88246d90f5eec
Ubuntu <ubuntu@ip-172-31-36-122.ap-southeast-1.compute.internal>
2019-09-18 10:44:21 +0000

    add resuseaddr to client

--- afl-protocol/network/utilities.c
+++ afl-protocol/network/utilities.c
@@ -55,6 +55,9 @@ int new_connection(const char *ip, unsigned int port)
     struct sockaddr_in servaddr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int opt_val = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
+
     bzero(&servaddr, sizeof(servaddr)); 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = inet_addr(ip); 
