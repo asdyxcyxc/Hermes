451eafbffe73b30c39574be865ef66c6a6732869
ping <trungstp@gmail.com>
2019-09-25 12:46:06 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 BINARY=$1
 if [ "$BINARY" = "simple" ]; then
-	cd afl-protocol && rm -rf ../sample/simple/out && BIND_DIR=/tmp/ USE_SOCKFD=3 USE_SIGSTOP=1 ./afl-fuzz -i ../sample/simple/input_dir -o ../sample/simple/out -p 4000 -h ./libhook.so -t 10000  -- ../sample/simple/server 4000
+	cd afl-protocol && rm -rf ../sample/simple/out && BIND_DIR=/tmp/ USE_SOCKFD=3 USE_SIGSTOP=1 ./afl-fuzz -i ../sample/simple/in -o ../sample/simple/out -p 4000 -h ./libhook.so -t 10000  -- ../sample/simple/server 4000
 fi
